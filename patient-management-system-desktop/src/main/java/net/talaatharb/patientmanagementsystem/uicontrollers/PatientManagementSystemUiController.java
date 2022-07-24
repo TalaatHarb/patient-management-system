931209322f118042550bfd88eeab6393369cd553
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-07-24 12:31:21 +0200

    added some UI for the desktop version

--- patient-management-system-desktop/src/main/java/net/talaatharb/patientmanagementsystem/uicontrollers/PatientManagementSystemUiController.java
+++ patient-management-system-desktop/src/main/java/net/talaatharb/patientmanagementsystem/uicontrollers/PatientManagementSystemUiController.java
@@ -2,9 +2,27 @@ package net.talaatharb.patientmanagementsystem.uicontrollers;
 
 import org.springframework.stereotype.Component;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import net.talaatharb.patientmanagementsystem.dtos.OrganizationDto;
+import net.talaatharb.patientmanagementsystem.facades.PatientManagementFacade;
 
 @Component
 @RequiredArgsConstructor
 public class PatientManagementSystemUiController {
+	
+	private final PatientManagementFacade patientManagementFacade;
+	
+	@FXML
+	@Setter
+	private TextField organizationName;
+	
+	@FXML
+	public void addOrganization() {
+		OrganizationDto organizationDto = new OrganizationDto();
+		organizationDto.setName(organizationName.getText());
+		patientManagementFacade.createOrganization(organizationDto);
+	}
 }
