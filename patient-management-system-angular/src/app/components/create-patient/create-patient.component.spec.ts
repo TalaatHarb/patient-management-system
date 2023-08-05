2c4183513a7203e46043d30d9be49431c540d448
mharb <mohamed.harb@cegedim.com>
2023-08-05 11:45:38 +0300

    fixed tests for new components in UI

--- patient-management-system-angular/src/app/components/create-patient/create-patient.component.spec.ts
+++ patient-management-system-angular/src/app/components/create-patient/create-patient.component.spec.ts
@@ -3,21 +3,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PatientMangamentService } from 'src/app/services/patient-mangament.service';
 
 import { CreatePatientComponent } from './create-patient.component';
+import { MockModule } from 'ng-mocks';
+import { FormsModule } from '@angular/forms';
+import { MedicalCenter } from 'src/app/models/medical-center.model';
+import { Observable } from 'rxjs';
+import { Organization } from 'src/app/models/organization.model';
+import { Patient } from 'src/app/models/patient.model';
 
 describe('CreatePatientComponent', () => {
   let component: CreatePatientComponent;
   let fixture: ComponentFixture<CreatePatientComponent>;
-  const patientManagementService: PatientMangamentService = jasmine.createSpyObj('patientManagementService', ['createOrganization']);
+  let patientManagementService: PatientMangamentService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CreatePatientComponent ],
-      imports: [HttpClientTestingModule],
-      providers: [
-        { provide: PatientMangamentService, useValue: patientManagementService }
-      ]
+      imports: [HttpClientTestingModule, MockModule(FormsModule)],
+      
     })
     .compileComponents();
+
+    fixture = TestBed.createComponent(CreatePatientComponent);
+    component = fixture.componentInstance;
+    patientManagementService = fixture.debugElement.injector.get(PatientMangamentService);
+
+    spyOn(patientManagementService, 'fetchOrganizations').and.returnValue(new Observable<Organization[]>());
+    spyOn(patientManagementService, 'fetchMedicalCenters').and.returnValue(new Observable<MedicalCenter[]>());
+    spyOn(patientManagementService, 'createPatient').and.returnValue(new Observable<Patient>());
+
   });
 
   beforeEach(() => {
--- patient-management-system-angular/src/app/components/list-organizations/list-organizations.component.spec.ts
+++ patient-management-system-angular/src/app/components/list-organizations/list-organizations.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListOrganizationsComponent } from './list-organizations.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { MockModule } from 'ng-mocks';
 
 describe('ListOrganizationsComponent', () => {
   let component: ListOrganizationsComponent;
@@ -8,7 +11,8 @@ describe('ListOrganizationsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ListOrganizationsComponent]
+      declarations: [ListOrganizationsComponent],
+      imports: [HttpClientTestingModule, MockModule(FormsModule)]
     });
     fixture = TestBed.createComponent(ListOrganizationsComponent);
     component = fixture.componentInstance;
